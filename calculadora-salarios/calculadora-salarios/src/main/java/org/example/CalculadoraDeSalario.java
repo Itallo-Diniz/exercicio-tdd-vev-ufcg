ec913077aff66e777bce46daae5f22734d131bb8
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2023-04-12 10:57:03 -0300

    refatorando a função de calcular salário

--- calculadora-salarios/calculadora-salarios/src/main/java/org/example/CalculadoraDeSalario.java
+++ calculadora-salarios/calculadora-salarios/src/main/java/org/example/CalculadoraDeSalario.java
@@ -3,29 +3,22 @@ package org.example;
 public class CalculadoraDeSalario {
 
     public static double calculaSalario(Funcionario funcionario) {
-        double salarioBase = funcionario.getSalarioBase();
-        double salarioLiquido = salarioBase;
-        Cargo cargoFuncionario = funcionario.getCargo();
+        double desconto = calculaDesconto(funcionario.getCargo(), funcionario.getSalarioBase());
+        return funcionario.getSalarioBase() - desconto;
+    }
 
-        if(cargoFuncionario.equals(Cargo.DESENVOLVEDOR)){
-            if (salarioBase >= 3000.0) {
-                salarioLiquido -= salarioBase * 0.2;
-            } else {
-                salarioLiquido -= salarioBase * 0.1;
-            }
-        }else if(cargoFuncionario.equals(Cargo.DBA) || cargoFuncionario.equals(Cargo.TESTADOR)){
-            if (salarioBase >= 2000.0) {
-                salarioLiquido -= salarioBase * 0.25;
-            } else {
-                salarioLiquido -= salarioBase * 0.15;
-            }
-        }else if (cargoFuncionario.equals(Cargo.GERENTE)){
-            if (salarioBase >= 5000.0) {
-                salarioLiquido -= salarioBase * 0.3; // desconto de 30%
-            } else {
-                salarioLiquido -= salarioBase * 0.2; // desconto de 20%
-            }
+    private static double calculaDesconto(Cargo cargo, double salarioBase){
+        double desconto;
+        if(cargo.equals(Cargo.DESENVOLVEDOR)){
+            desconto = salarioBase >= 3000.0 ?  salarioBase * 0.2 :  salarioBase * 0.1;
+        }else if(cargo.equals(Cargo.DBA) || cargo.equals(Cargo.TESTADOR)){
+            desconto = salarioBase >= 2000.0 ?  salarioBase * 0.25 :  salarioBase * 0.15;
+        }else if (cargo.equals(Cargo.GERENTE)){
+            desconto = salarioBase >= 5000.0 ?  salarioBase * 0.3 :  salarioBase * 0.2;
+        }else{
+            desconto = 0.0;
         }
-        return salarioLiquido;
+
+        return desconto;
     }
 }
