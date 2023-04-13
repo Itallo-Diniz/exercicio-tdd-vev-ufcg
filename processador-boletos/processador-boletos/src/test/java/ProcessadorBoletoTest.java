4c2013368aef1f9ee915ab181fb4eb04cfc75062
José Diniz <jose.diniz@ccc.ufcg.edu.br>
2023-04-12 22:03:00 -0300

    teste para verificar se a fatura é considerada paga quando inicia com o valor vazio

--- processador-boletos/processador-boletos/src/test/java/ProcessadorBoletoTest.java
+++ processador-boletos/processador-boletos/src/test/java/ProcessadorBoletoTest.java
@@ -3,6 +3,8 @@ import org.example.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 class ProcessadorBoletoTest extends Main {
@@ -11,5 +13,15 @@ class ProcessadorBoletoTest extends Main {
     public void verificaSeBoletosIniciaVazio() {
         ArrayList<Boleto> boletos = new ArrayList<Boleto>();
         Assertions.assertEquals(0,  boletos.size());
+    }
+
+    @Test
+    public void deveMarcarFaturaComoPagaQuandoFaturaZerada() {
+        LocalDate data = LocalDate.of(2023, 4, 12);
+        Double valorTotal = 0.0;
+        String nomeCliente = "José Itallo";
+        Fatura fatura = new Fatura(data, valorTotal, nomeCliente);
+        
+        Assertions.assertTrue(fatura.isPaga());
     }
 }
