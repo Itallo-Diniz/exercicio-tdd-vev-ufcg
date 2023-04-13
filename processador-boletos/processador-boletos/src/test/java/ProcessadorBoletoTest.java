75909db9f74910becd162b8334cd19ad94fe2546
José Diniz <jose.diniz@ccc.ufcg.edu.br>
2023-04-12 21:45:32 -0300

    fix testa lista de boletos vazia

--- processador-boletos/processador-boletos/src/test/java/ProcessadorBoletoTest.java
+++ processador-boletos/processador-boletos/src/test/java/ProcessadorBoletoTest.java
@@ -1,16 +1,15 @@
+import org.example.Boletos;
 import org.example.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ProcessadorBoletoTest extends Main {
 
     @Test
     public void verificaSeBoletosIniciaVazio() {
-        ArrayList<Boletos> boletos = new ArrayList<Object>();
+        ArrayList<Boletos> boletos = new ArrayList<Boletos>();
         Assertions.assertEquals(0,  boletos.size());
     }
 }
